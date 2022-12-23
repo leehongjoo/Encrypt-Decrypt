05bac7ca35a61c08e28a8ca5d7b0f4eb10735e2a
leehongjoo <leehj190@naver.com>
2022-12-23 15:04:36 +0900

    Add bqi, mqi
    Add bqi, mqi

--- Encrypt-Decrypt/Assets/Scripts/Encrypt_Decrypt.cs
+++ Encrypt-Decrypt/Assets/Scripts/Encrypt_Decrypt.cs
@@ -6,6 +6,7 @@ using System.IO;
 using UnityEngine;
 using System.Runtime.InteropServices;
 using System;
+using System.Text;
 
 public class Encrypt_Decrypt : MonoBehaviour
 {
@@ -15,7 +16,7 @@ public class Encrypt_Decrypt : MonoBehaviour
     public TMP_Text ChannelNumber;
     public TMP_Text samplingRate;
     public TMP_Text TotalSample;
-    public TMP_Text EEGData;
+    public TMP_Text StatusText;
 
     private string EquipmentNameString;
     private string StartTimeString;
@@ -25,6 +26,12 @@ public class Encrypt_Decrypt : MonoBehaviour
     private string TotalSampleString;
     private string EEGDataString = "";
     private PED textData;
+
+    private string plainString;
+
+    private string bfnString = "";
+    private string encryptString;
+
     public void TxtFileLoad()
     {
 #if UNITY_STANDALONE_WIN
@@ -61,6 +68,7 @@ public class Encrypt_Decrypt : MonoBehaviour
                 line = "파일이 없습니다";
             }
         });
+        StatusText.text = " Load 완료";
 #endif
     }
     public void EncryptPEDFile()
@@ -83,7 +91,8 @@ public class Encrypt_Decrypt : MonoBehaviour
                 directoryInfo.Create();
             }
             CryptManager.SavePEDFile(ped, path);
-        });
+            StatusText.text = " 암호화 & PED 파일 저장 완료";
+        });        
     }
     public void PEDFileDecrypt()
     {
@@ -105,9 +114,7 @@ public class Encrypt_Decrypt : MonoBehaviour
                 ChannelNumber.text = textData.ChannelNumber;
                 samplingRate.text = textData.SamplingRate;
                 TotalSample.text = textData.TotalSample;
-                Debug.Log(textData.EEGData[0]);
-                Debug.Log(textData.EEGData[1]);
-                Debug.Log(textData.EEGData[2]);
+                StatusText.text = " PED 파일 Load 완료";
             }
         });
 #endif
@@ -139,6 +146,153 @@ public class Encrypt_Decrypt : MonoBehaviour
             writer.WriteLine();
             writer.WriteLine(textData.EEGData);
             writer.Close();
+            StatusText.text = " Decrypt 완료 & Txt 파일 Save 완료";
+        });
+    }
+
+    public void bfnFileLoad()
+    {
+#if UNITY_STANDALONE_WIN
+        var bp = new BrowserProperties();
+        bp.filter = "bfn files (*.bfn)|*.bfn";
+        bp.filterIndex = 0;
+        bp.initialDir = @"C:\cmd";
+        bfnString = "";
+        new fileBrowser().OpenFileBrowser(bp, result =>
+        {
+            FileInfo fileInfo = new FileInfo(result);
+            string line = "";
+            if (fileInfo.Exists)
+            {
+                StreamReader reader = new StreamReader(result);
+                while ((line = reader.ReadLine()) != null)
+                {
+                    bfnString += line;
+                    bfnString += "\n";
+                }
+                bfnString = bfnString.Substring(0, bfnString.Length - 1);
+                reader.Close();
+                Debug.Log(bfnString);
+            }
+            else
+            {
+                line = "파일이 없습니다";
+            }
+        });
+#endif
+    }
+    public void EncryptbfnFile()
+    {
+        encryptString = AESCrypto.AESEncrypt128(bfnString);
+        Debug.Log(encryptString);
+    }
+    public void bfnFileDecrypt()
+    {
+        string result = AESCrypto.AESDecrypt128(encryptString);
+        Debug.Log(result);
+    }
+    public void DecryptDataMakeTxtFile2()
+    {
+
+    }
+    public void bqiFileLoad()
+    {
+#if UNITY_STANDALONE_WIN
+        var bp = new BrowserProperties();
+        bp.filter = "bqi mqi files (*.bqi)|*.bqi|(*.mqi)|*.mqi";
+        bp.filterIndex = 0;
+        bp.initialDir = @"C:\cmd";
+        EEGDataString = "";
+        new fileBrowser().OpenFileBrowser(bp, result =>
+        {
+            FileInfo fileInfo = new FileInfo(result);
+            string line = "";
+            if (fileInfo.Exists)
+            {
+                StreamReader reader = new StreamReader(result);
+                line = reader.ReadLine();
+                plainString = line;
+                StatusText.text = " Load 완료";
+                reader.Close();
+            }
+            else
+            {
+                // 에러 코드 
+                line = "파일이 없습니다";
+            }
+        });
+#endif
+    }
+    public void LoadDataTotxtFile()
+    {
+        var extensionList = new[]
+        {
+            new ExtensionFilter("EEGData", "txt"),
+        };
+        StandaloneFileBrowser.SaveFilePanelAsync("Save File", "", "", extensionList, (string path) =>
+        {
+            if (path == "")
+                return;
+            DirectoryInfo directoryInfo = new DirectoryInfo(Path.GetDirectoryName(path));
+            if (!directoryInfo.Exists)
+            {
+                directoryInfo.Create();
+            }
+            StreamWriter sw = new StreamWriter(path, true, Encoding.UTF8);
+            string data = AESCrypto.AESDecrypt128(plainString);
+            sw.Write(data);
+            sw.Close();
+            StatusText.text = " txt로 변경 저장 완료";
+        });
+    }
+    public void bqfFileLoad()
+    {
+#if UNITY_STANDALONE_WIN
+        var bp = new BrowserProperties();
+        bp.filter = "bqf files (*.bqf)|*.bqf";
+        bp.filterIndex = 0;
+        bp.initialDir = @"C:\cmd";
+        EEGDataString = "";
+        new fileBrowser().OpenFileBrowser(bp, result =>
+        {
+            FileInfo fileInfo = new FileInfo(result);
+            string line = "";
+            if (fileInfo.Exists)
+            {
+                StreamReader reader = new StreamReader(result);
+                line = reader.ReadLine();
+                plainString = line;
+                StatusText.text = "bqf Load 완료";
+                reader.Close();
+            }
+            else
+            {
+                // 에러 코드 
+                line = "파일이 없습니다";
+            }
+        });
+#endif
+    }
+    public void bqfLoadDataTotxtFile()
+    {
+        var extensionList = new[]
+        {
+            new ExtensionFilter("EEGData", "txt"),
+        };
+        StandaloneFileBrowser.SaveFilePanelAsync("Save File", "", "", extensionList, (string path) =>
+        {
+            if (path == "")
+                return;
+            DirectoryInfo directoryInfo = new DirectoryInfo(Path.GetDirectoryName(path));
+            if (!directoryInfo.Exists)
+            {
+                directoryInfo.Create();
+            }
+            StreamWriter sw = new StreamWriter(path, true, Encoding.UTF8);
+            string data = AESCrypto.AESDecrypt128(plainString);
+            sw.Write(data);
+            sw.Close();
+            StatusText.text = " txt로 변경 저장 완료";
         });
     }
 }
